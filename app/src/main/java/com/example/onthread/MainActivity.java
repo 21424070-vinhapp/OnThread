92daf6167fe870bd11c53f45cc404a54533942c3
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-18 16:05:28 +0700

    synchronized

--- app/src/main/java/com/example/onthread/MainActivity.java
+++ app/src/main/java/com/example/onthread/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.onthread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,12 +29,25 @@ public class MainActivity extends AppCompatActivity {
 
         thread1.start();
         thread2.start();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("TAG", thread1.getState().toString());
+                Log.d("TAG", thread2.getState().toString());
+            }
+        },1000);
     }
 
-    private void printLog(String b) {
-        for(int i=0;i<10;i++)
+    private synchronized void printLog(String b) {
+        for(int i=0;i<100;i++)
         {
             Log.d("BBB", b+": "+i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
