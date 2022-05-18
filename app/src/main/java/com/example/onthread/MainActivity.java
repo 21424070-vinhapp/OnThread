e7f2e3156abafc4a59323fcdd32d2fe2dfce16c1
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-18 15:52:23 +0700

    vi du ve xu ly song song cua thread

--- app/src/main/java/com/example/onthread/MainActivity.java
+++ app/src/main/java/com/example/onthread/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.onthread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread1=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                printLog("A");
+            }
+        });
+
+        Thread thread2=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                printLog("B");
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+
+    private void printLog(String b) {
+        for(int i=0;i<10;i++)
+        {
+            Log.d("BBB", b+": "+i);
+        }
     }
 }
